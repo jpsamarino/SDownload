b92f3b2b9d523a02a0893cc9e2b6213db5a701f8
jpsamarino <jpsamarino3@hotmail.com>
2025-04-20 00:15:36 -0300

    refactor: improving implementation

--- sDownload/utils/url_to_file_name.py
+++ sDownload/utils/url_to_file_name.py
@@ -10,9 +10,9 @@ def url_to_file_name(url: str, max_length: int = 100) -> str:
         return common_name
 
     relevant = f"{parsed.path}_{parsed.query}" if parsed.query else parsed.path
-    filename = re.sub(r'[^a-zA-Z0-9]+', '_', relevant).strip('_')
-
-    if filename == "":
-        filename = re.sub(r'[^a-zA-Z0-9]+', '_', parsed.netloc).strip('_')
-
+    filename = safe_slugify(relevant) or safe_slugify(parsed.netloc)
     return filename[-max_length:] + ".bin"
+
+
+def safe_slugify(s: str) -> str:
+    return re.sub(r'[^a-zA-Z0-9]+', '_', s).strip('_')
