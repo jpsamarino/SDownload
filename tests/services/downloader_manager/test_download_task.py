845c6d1ea9ed6eb9099fea2f7dca355a37edd302
jpsamarino <jpsamarino3@hotmail.com>
2025-05-08 22:02:20 -0300

    feat: add initial tests to DownloadTask

--- tests/services/downloader_manager/test_download_task.py
+++ tests/services/downloader_manager/test_download_task.py
@@ -3,6 +3,9 @@ import logging
 import pytest
 from datetime import datetime
 
+from sDownload.file_system.local_storage import LocalStorage
+from sDownload.http_client.httpx_downloader import HttpxDownloader
+from sDownload.interfaces.protocols.http_config_model import HttpConfigModel
 from sDownload.services.downloader_manager.download_task import DownloadConfig, DownloadTask
 
 
@@ -21,6 +24,46 @@ class DummyStorage:
     async def save_binary_data(self, it, name):
         async for _ in it:
             pass
+
+
+@pytest.fixture
+def storage(tmp_path: str):
+    return LocalStorage(storage_dir="./delet")
+
+
+@pytest.mark.asyncio
+async def test_httpx_and_dowload_task(nginx_custom, storage):
+    config = HttpConfigModel(timeout_connect=20.0)
+    downloader = HttpxDownloader(config)
+    result_list = await downloader.get_file_info(f"{nginx_custom['http']}/limited_speed/file_100k.bin")
+    result = result_list[0]
+    assert result.file_name == "file_100k.bin"
+    assert result.file_size == 102400
+
+    config = DownloadConfig(
+        file_name=result.file_name,
+        file_dir=result.file_dir,  # remove
+        file_size=result.file_size,
+        file_id=result.file_id,
+        download_url=result.download_url,
+        file_created_at=datetime.utcnow(),
+        protocol_data=None,
+        max_connections_per_download=10,
+    )
+
+    task = DownloadTask(
+        cfg=config,
+        downloader=downloader,
+        storage=storage,
+    )
+    await task.start()
+
+    # verify if the file was downloaded and the size is correct
+    received = b""
+    async for chunk in storage.get_binary_data(config.file_name):
+        received += chunk
+
+    assert len(received) == config.file_size
 
 
 @pytest.mark.asyncio
