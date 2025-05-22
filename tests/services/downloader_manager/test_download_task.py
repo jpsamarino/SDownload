dfa499f9bea9711fe1032c719c7d802748443588
jpsamarino <jpsamarino3@hotmail.com>
2025-05-22 10:16:43 -0300

    feat: test download task with real servers

--- tests/services/downloader_manager/test_download_task.py
+++ tests/services/downloader_manager/test_download_task.py
@@ -21,14 +21,68 @@ class DummyDownloader:
 
 
 class DummyStorage:
-    async def save_binary_data(self, it, name):
+    async def save_binary_data(self, name, it):
         async for _ in it:
             pass
+
+    async def merge_binary_files(self, arg1, arg2):
+        pass
+
+    async def delete_data(self, arg1):
+        pass
 
 
 @pytest.fixture
 def storage(tmp_path: str):
     return LocalStorage(storage_dir="./delet")
+# https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/2025-03/Estabelecimentos9.zip
+# https://ftp.dadosabertos.ans.gov.br/FTP/PDA/dados_de_beneficiarios_por_operadora/sib_ativo_MG.zip
+# https://storage.live.com/downloadfiles/V1/Zip?application=1141147648&authkey=!ANB8GHSnGvZzWFM
+
+
+@pytest.mark.asyncio
+async def test_httpx_and_dowload_task_real(storage):
+    config = HttpConfigModel(timeout_connect=20.0, valid_ssl=False)
+    downloader = HttpxDownloader(config)
+    # result_list = await downloader.get_file_info(f"https://storage.live.com/downloadfiles/V1/Zip?application=1141147648&authkey=!ANB8GHSnGvZzWFM")
+    result_list = await downloader.get_file_info(f"https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/2025-03/Estabelecimentos9.zip")
+    # result_list = await downloader.get_file_info(f"https://ftp.dadosabertos.ans.gov.br/FTP/PDA/dados_de_beneficiarios_por_operadora/sib_ativo_MG.zip")
+    result = result_list[0]
+
+    config = DownloadConfig(
+        file_name=result.file_name,
+        file_dir=result.file_dir,  # remove
+        file_size=result.file_size,
+        file_id=result.file_id,
+        download_url=result.download_url,
+        file_created_at=datetime.utcnow(),
+        protocol_data=None,
+        max_connections_per_download=10,
+        max_speed_bytes_per_second=1024*1024*10,
+    )
+
+    logger = logging.getLogger("download-task")
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    task = DownloadTask(
+        cfg=config,
+        downloader=downloader,
+        storage=storage,
+        logger=logger,
+    )
+    task.start()
+    await task.wait_util_done()
+
+    # # verify if the file was downloaded and the size is correct
+    # received = b""
+    # async for chunk in storage.get_binary_data(config.file_name):
+    #     received += chunk
+
+    # assert len(received) == config.file_size
 
 
 @pytest.mark.asyncio
@@ -48,7 +102,7 @@ async def test_httpx_and_dowload_task(nginx_custom, storage):
         download_url=result.download_url,
         file_created_at=datetime.utcnow(),
         protocol_data=None,
-        max_connections_per_download=10,
+        max_connections_per_download=2,
     )
 
     task = DownloadTask(
