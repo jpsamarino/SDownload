935d3a37ba4ffbc7813717fff6412156974e2d5f
jpsamarino <jpsamarino3@hotmail.com>
2026-01-16 18:51:41 -0500

    feat: add length property in ChunkRange

--- sDownload/interfaces/protocols/chunk_models.py
+++ sDownload/interfaces/protocols/chunk_models.py
@@ -5,6 +5,15 @@ class ChunkRange(NamedTuple):
     start: int
     end: int | None
 
+    def __str__(self) -> str:
+        return f"{self.start}_{self.end or 'EOF'}"
+
+    @property
+    def length(self) -> int | None:
+        if self.end is None:
+            return None
+        return self.end - self.start + 1
+
 
 class ChunkOperationPlanModel(TypedDict):
     chunks_to_start: list[ChunkRange] | None
